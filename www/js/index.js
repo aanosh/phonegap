9bd1b9bc74f4ae688e4e6df4a35922482785c524
Artem Anoshenko <artem.anoshenko@valant.com.ua>
2014-04-23 19:10:04 +0300

    +

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,19 @@ function getImage() {
     alert(11);
 }
 
+function scanBarcode() {
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+}
 
 //        function getImage() {
 //            // Открываем галерею
