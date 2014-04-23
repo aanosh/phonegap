4e9041fca00dbf85124dde5eae85aca7ba5a854f
Artem Anoshenko <artem.anoshenko@valant.com.ua>
2014-04-23 19:53:47 +0300

    +

--- www/js/index.js
+++ www/js/index.js
@@ -26,37 +26,32 @@ function scanBarcode() {
     }
 
     function uploadPhoto(imageURI) {
-        alert(imageURI);
+            var options = new FileUploadOptions();
+            options.fileKey="file";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.mimeType="image/jpeg";
+ 
+            var params = new Object();
+            params.value1 = "test";
+            params.value2 = "param";
+ 
+            options.params = params;
+            options.chunkedMode = false;
+ 
+            var ft = new FileTransfer();
+            ft.upload(imageURI, "http://demo.testindev.com/", win, fail, options);
     }
-    
+
+
+        //если все хорошо
+        function win(r) {
+            console.log("Code = " + r.responseCode);
+            console.log("Response = " + r.response);
+            console.log("Sent = " + r.bytesSent);
+            alert(r.response);
+        }
         
-//        //загружаем картинки на сервер
-//        function uploadPhoto(imageURI) {
-//            var options = new FileUploadOptions();
-//            options.fileKey="file";
-//            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
-//            options.mimeType="image/jpeg";
-// 
-//            var params = new Object();
-//            params.value1 = "test";
-//            params.value2 = "param";
-// 
-//            options.params = params;
-//            options.chunkedMode = false;
-// 
-//            var ft = new FileTransfer();
-//            ft.upload(imageURI, "http://yourdomain.com/upload.php", win, fail, options);
-//        }
-//        
-//        //если все хорошо
-//        function win(r) {
-//            console.log("Code = " + r.responseCode);
-//            console.log("Response = " + r.response);
-//            console.log("Sent = " + r.bytesSent);
-//            alert(r.response);
-//        }
-//        
-//        //если ошибка
-//        function fail(error) {
-//            alert("An error has occurred: Code = " + error.code);
-//        }
+        //если ошибка
+        function fail(error) {
+            alert("An error has occurred: Code = " + error.code);
+        }
