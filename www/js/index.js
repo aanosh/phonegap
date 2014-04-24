cc8f71ed9ff5be0e4e6aab4e18ea7599482de137
Artem Anoshenko <artem.anoshenko@valant.com.ua>
2014-04-24 16:08:06 +0300

    +

--- www/js/index.js
+++ www/js/index.js
@@ -5,7 +5,6 @@ function scanBarcode() {
                   "Result: " + result.text + "\n" +
                   "Format: " + result.format + "\n" +
                   "Cancelled: " + result.cancelled);
-          return false;
         }, 
         function (error) {
             alert("Scanning failed: " + error);
@@ -13,45 +12,45 @@ function scanBarcode() {
     );
 }
 
-    function getImage() {
-        // Открываем галерею
-        navigator.camera.getPicture(uploadPhoto, function(message) {
-                    alert(message);
-            },{
-                    quality: 50, 
-                    destinationType: navigator.camera.DestinationType.FILE_URI,
-                    sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
-            }
-        );
-
-    }
-
-    function uploadPhoto(imageURI) {
-            var options = new FileUploadOptions();
-            options.fileKey="file";
-            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
-            options.mimeType="image/jpeg";
- 
-            var params = new Object();
-            params.value1 = "test";
-            params.value2 = "param";
- 
-            options.params = params;
-            options.chunkedMode = false;
-            var ft = new FileTransfer();
-            ft.upload(imageURI, "http://demo.testindev.com/", win, fail, options);
-    }
-
-
-        //если все хорошо
-        function win(r) {
-            console.log("Code = " + r.responseCode);
-            console.log("Response = " + r.response);
-            console.log("Sent = " + r.bytesSent);
-            alert(r.response);
-        }
-        
-        //если ошибка
-        function fail(error) {
-            alert("An error has occurred: Code = " + error.code);
-        }
+//    function getImage() {
+//        // Открываем галерею
+//        navigator.camera.getPicture(uploadPhoto, function(message) {
+//                    alert(message);
+//            },{
+//                    quality: 50, 
+//                    destinationType: navigator.camera.DestinationType.FILE_URI,
+//                    sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+//            }
+//        );
+//
+//    }
+//
+//    function uploadPhoto(imageURI) {
+//            var options = new FileUploadOptions();
+//            options.fileKey="file";
+//            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+//            options.mimeType="image/jpeg";
+// 
+//            var params = new Object();
+//            params.value1 = "test";
+//            params.value2 = "param";
+// 
+//            options.params = params;
+//            options.chunkedMode = false;
+//            var ft = new FileTransfer();
+//            ft.upload(imageURI, "http://demo.testindev.com/", win, fail, options);
+//    }
+//
+//
+//        //если все хорошо
+//        function win(r) {
+//            console.log("Code = " + r.responseCode);
+//            console.log("Response = " + r.response);
+//            console.log("Sent = " + r.bytesSent);
+//            alert(r.response);
+//        }
+//        
+//        //если ошибка
+//        function fail(error) {
+//            alert("An error has occurred: Code = " + error.code);
+//        }
