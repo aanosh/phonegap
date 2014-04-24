75d49ad2e535de6ac0e6f7341ccc6a5a04b2f41b
Artem Anoshenko <artem.anoshenko@valant.com.ua>
2014-04-24 17:24:07 +0300

    +

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,11 @@
 function scanBarcode() {
     cordova.plugins.barcodeScanner.scan(
         function (result) {
-            alert("We got a barcode\n" +
-                  "Result: " + result.text + "\n" +
-                  "Format: " + result.format + "\n" +
-                  "Cancelled: " + result.cancelled);
+//            alert("We got a barcode\n" +
+//                  "Result: " + result.text + "\n" +
+//                  "Format: " + result.format + "\n" +
+//                  "Cancelled: " + result.cancelled);
+            navigator.notification.alert("Result: " + result.text + "\n", function(){}, "", "");
         }, 
         function (error) {
             alert("Scanning failed: " + error);
